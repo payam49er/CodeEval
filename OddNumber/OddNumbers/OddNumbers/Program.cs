b6cd89166c0cf2a50af869534c7131bd50bf23bd
Payam <payam.shoghi@me.com>
2014-06-26 19:54:13 -0400

    Solution to https://www.codeeval.com/open_challenges/25/

---
+++ OddNumber/OddNumbers/OddNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>
+/// Solution to https://www.codeeval.com/open_challenges/25/
+/// Print the odd numbers from 1 to 99.
+/// </summary>
+namespace OddNumbers
+{
+	class MainClass
+	{
+
+		public static void Main (string[] args)
+		{
+			for(int i= 1; i < 100; i++)
+			{
+				if(i %2 != 0)
+					Console.WriteLine (i);
+			}
+
+			Console.Read ();
+		}
+	}
+}
