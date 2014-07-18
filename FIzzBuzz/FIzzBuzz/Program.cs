cb9d0e8973bdd560c21ce7b87b2c66a63136c1fa
Payam <payam.shoghi@me.com>
2014-07-17 23:55:40 -0400

    Solution to Fizzbuzz for Code Eval

---
+++ FIzzBuzz/FIzzBuzz/Program.cs
@@ -0,0 +1,76 @@
+﻿///This code is written exclusively for CodeEval
+///Challenge FizzBuzz https://www.codeeval.com/open_challenges/1/
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{    
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                using (StreamReader reader = File.OpenText(args[0]))
+                {
+
+                    List<string[]> values = new List<string[]>();
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (line != null)
+                        {
+                            var value = line.Split(',');
+                            values.Add(value);
+                        }
+
+                    }
+
+                    foreach (var lineOfnumber in values)
+                    {
+                        int A;
+                        int.TryParse(lineOfnumber[0], out A);
+
+                        int B;
+                        int.TryParse(lineOfnumber[1], out B);
+
+                        int N;
+                        int.TryParse(lineOfnumber[2], out N);
+
+                        for (int i = 1; i < N + 1; i++)
+                        {
+                            if (i%A == 0 && i%B == 0)
+                                Console.Write("FB" + " ");
+                            else if (i%A == 0)
+
+                                Console.Write("F" + " ");
+
+                            else if (i%B == 0)
+
+                                Console.Write("B" + " ");
+
+                            else
+
+                                Console.Write(i + " ");
+
+                        }
+
+                        Console.WriteLine();
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(string.Format("Your input path is in correct \n" + e));
+            }
+        }
+    }
+}
